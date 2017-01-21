bb5f2d32539f5da866314ba7dbf709beb8f4eb73
codec-abc <noemail>
2017-01-21 17:47:54 +0100

    add sound and clean a little bit

---
+++ Assets/Scripts/JumpSoundsProvider.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class JumpSoundsProvider : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip[] jumpSounds;
+
+    public AudioClip[] GetJumpSounds()
+    {
+        return jumpSounds;
+    }
+}
--- Assets/Scripts/player_cpma.cs
+++ Assets/Scripts/player_cpma.cs
@@ -27,95 +27,81 @@ public class player_cpma : MonoBehaviour
 
     /* Player view stuff */
 
+    [SerializeField]
+    Transform jumpSoundsObj;
+    JumpSoundsProvider jumpSoundsProvider;
+
+    [SerializeField]
     Transform playerView;  // Must be a camera
-    float playerViewYOffset = 0.6f; // The height at which the camera is bound to
-    float xMouseSensitivity = 30.0f;
-    float yMouseSensitivity = 30.0f;
+
+    float xMouseSensitivity = 10.0f;
+    float yMouseSensitivity = 10.0f;
 
     /* Frame occuring factors */
-    float gravity  = 20.0f;
-    float friction = 6f;                // Ground friction
+    float gravity = 20.0f;
+    float friction = 50f;  // Ground friction
 
     /* Movement stuff */
-    float moveSpeed              = 7.0f;  // Ground move speed
-    float runAcceleration        = 14f;   // Ground accel
-    float runDeacceleration      = 10f;   // Deacceleration that occurs when running on the ground
-    float airAcceleration        = 2.0f;  // Air accel
-    float airDeacceleration      = 2.0f;    // Deacceleration experienced when opposite strafing
-    float airControl             = 0.3f;  // How precise air control is
+    float moveSpeed = 50.0f;  // Ground move speed
+    float runAcceleration = 14f;   // Ground accel
+    float runDeacceleration = 10f;   // Deacceleration that occurs when running on the ground
+
+    float airAcceleration = 2.0f;  // Air accel
+    float airDeacceleration = 2.0f;    // Deacceleration experienced when opposite strafing
+    float airControl = 0.3f;  // How precise air control is
+
     float sideStrafeAcceleration = 50f;   // How fast acceleration occurs to get up to sideStrafeSpeed when side strafing
-    float sideStrafeSpeed        = 1f;    // What the max speed to generate when side strafing
-    float jumpSpeed              = 8.0f;  // The speed at which the character's up axis gains when hitting jump
-    float moveScale              = 1.0f;
+    float sideStrafeSpeed = 1f;    // What the max speed to generate when side strafing
 
-    /* print() styles */
-    GUIStyle style;
-
-    /* Sound stuff */
-    AudioClip[] jumpSounds;
+    float jumpSpeed = 8.0f;  // The speed at which the character's up axis gains when hitting jump
+    float moveScale = 1.0f;
 
     /* FPS Stuff */
     float fpsDisplayRate = 4.0f;  // 4 updates per sec.
 
     private int frameCount = 0;
     private float dt = 0.0f;
-    private float fps = 0.0f;
 
+    [SerializeField]
     private CharacterController controller;
 
     // Camera rotationals
     private float rotX = 0.0f;
     private float rotY = 0.0f;
 
-    private Vector3 moveDirection = Vector3.zero;
-    private Vector3 moveDirectionNorm = Vector3.zero;
-    private Vector3 playerVelocity= Vector3.zero;
+    private Vector3 playerVelocity = Vector3.zero;
     private float playerTopVelocity = 0.0f;
-
-    // If true then the player is fully on the ground
-    private bool grounded = false;
 
     // Q3: players can queue the next jump just before he hits the ground
     private bool wishJump = false;
-
-    // Used to display real time friction values
-    private float playerFriction = 0.0f;
 
     // Contains the command the user wishes upon the character
     class Cmd
     {
         public float forwardmove;
-	    public float rightmove;
-	    public float upmove;
+        public float rightmove;
+        public float upmove;
     }
 
     private Cmd cmd; // Player commands, stores wish commands that the player asks for (Forward, back, jump, etc)
 
-    private Vector3 playerSpawnPos ;
-    private Quaternion playerSpawnRot ;
-
     private void Start()
     {
         /* Hide the cursor */
-        //Screen.showCursor = false;
         Cursor.visible = false;
-
-        //Screen.lockCursor = true;
         Cursor.lockState = CursorLockMode.Locked;
 
-        /* Put the camera inside the capsule collider */
-        //playerView.position = this.transform.position;
-        playerView.position = new Vector3 (transform.position.x, transform.position.y + playerViewYOffset, transform.position.z);
-
-        controller = GetComponent<CharacterController>();// GetComponent(CharacterController);
+        controller = GetComponent<CharacterController>();
         cmd = new Cmd();
 
-        // Set the spawn position of the player
-        playerSpawnPos = transform.position;
-        playerSpawnRot = playerView.rotation;
+        var instanciated = Instantiate(jumpSoundsObj, Vector3.zero, Quaternion.identity);
+        instanciated.parent = this.transform;
+        jumpSoundsProvider = instanciated.GetComponent<JumpSoundsProvider>();
+        Debug.Log(jumpSoundsProvider);
+        Debug.Assert(jumpSoundsProvider != null);
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         /* Do FPS calculation */
         frameCount++;
@@ -123,25 +109,31 @@ public class player_cpma : MonoBehaviour
 
         if (dt > 1.0f / fpsDisplayRate)
         {
-            fps = Mathf.Round(frameCount / dt);
             frameCount = 0;
             dt -= 1.0f / fpsDisplayRate;
         }
 
         /* Ensure that the cursor is locked into the screen */
-        //if (Screen.lockCursor == false)
         if (Cursor.lockState != CursorLockMode.Locked)
         {
             if (Input.GetMouseButtonDown(0))
             {
-                //Screen.lockCursor = true;
+                Cursor.visible = false;
                 Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
             }
         }
 
         /* Camera rotation stuff, mouse controls this shit */
-        rotX -= Input.GetAxis("Mouse Y") * xMouseSensitivity * 0.02f;
-        rotY += Input.GetAxis("Mouse X") * yMouseSensitivity * 0.02f;
+        rotX -= Input.GetAxis("Mouse Y") * xMouseSensitivity;
+        rotY += Input.GetAxis("Mouse X") * yMouseSensitivity;
 
         // Clamp the X rotation
         if (rotX < -90)
@@ -156,12 +148,6 @@ public class player_cpma : MonoBehaviour
         transform.rotation = Quaternion.Euler(0, rotY, 0); // Rotates the collider
         playerView.rotation = Quaternion.Euler(rotX, rotY, 0); // Rotates the camera
 
-        // Set the camera's position to the transform
-        //playerView.position = transform.position;
-        //playerView.position.y = transform.position.y + playerViewYOffset;
-
-        playerView.position = new Vector3 (transform.position.x, transform.position.y + playerViewYOffset, transform.position.z);
-
         /* Movement, here's the important part */
         QueueJump();
 
@@ -169,7 +155,7 @@ public class player_cpma : MonoBehaviour
         {
             GroundMove();
         }
-        else if (!controller.isGrounded)
+        else
         {
             AirMove();
         }
@@ -192,18 +178,18 @@ public class player_cpma : MonoBehaviour
     |* MOVEMENT
     \*******************************************************************************************************/
 
-    /**
-     * Sets the movement direction based on player input
-     */
+    /// <summary>
+    /// Sets the movement direction based on player input
+    /// </summary>
     private void SetMovementDir()
     {
         cmd.forwardmove = Input.GetAxis("Vertical");
         cmd.rightmove = Input.GetAxis("Horizontal");
     }
 
-    /**
-     * Queues the next jump just like in Q3
-     */
+    /// <summary>
+    /// Queues the next jump just like in Q3
+    /// </summary>
     private void QueueJump()
     {
         if (Input.GetKeyDown(KeyCode.Space) && !wishJump)
@@ -216,13 +202,12 @@ public class player_cpma : MonoBehaviour
         }
     }
 
-    /**
-     * Execs when the player is in the air
-     */
+    /// <summary>
+    /// Execs when the player is in the air
+    /// </summary>
     private void AirMove()
     {
         Vector3 wishdir;
-        float wishvel = airAcceleration;
         float accel;
 
         var scale = CmdScale();
@@ -236,7 +221,6 @@ public class player_cpma : MonoBehaviour
         wishspeed *= moveSpeed;
 
         wishdir.Normalize();
-        moveDirectionNorm = wishdir;
         wishspeed *= scale;
 
         // CPM: Aircontrol
@@ -274,11 +258,13 @@ public class player_cpma : MonoBehaviour
         // LEGACY MOVEMENT SEE BOTTOM
     }
 
-    /**
-     * Air control occurs when the player is in the air, it allows
-     * players to move side to side much faster rather than being
-     * 'sluggish' when it comes to cornering.
-     */
+    /// <summary>
+    /// * Air control occurs when the player is in the air, it allows
+    /// players to move side to side much faster rather than being
+    /// 'sluggish' when it comes to cornering.
+    /// </summary>
+    /// <param name="wishdir"></param>
+    /// <param name="wishspeed"></param>
     private void AirControl(Vector3 wishdir, float wishspeed)
     {
         float zspeed;
@@ -311,7 +297,6 @@ public class player_cpma : MonoBehaviour
             playerVelocity.z = playerVelocity.z * speed + wishdir.z * k;
 
             playerVelocity.Normalize();
-            moveDirectionNorm = playerVelocity;
         }
 
         playerVelocity.x *= speed;
@@ -320,9 +305,9 @@ public class player_cpma : MonoBehaviour
 
     }
 
-    /**
-     * Called every frame when the engine detects that the player is on the ground
-     */
+    /// <summary>
+    /// Called every frame when the engine detects that the player is on the ground
+    /// </summary>
     private void GroundMove()
     {
         Vector3 wishdir;
@@ -345,7 +330,6 @@ public class player_cpma : MonoBehaviour
         wishdir = new Vector3(cmd.rightmove, 0, cmd.forwardmove);
         wishdir = transform.TransformDirection(wishdir);
         wishdir.Normalize();
-        moveDirectionNorm = wishdir;
 
         var wishspeed = wishdir.magnitude;
         wishspeed *= moveSpeed;
@@ -363,9 +347,10 @@ public class player_cpma : MonoBehaviour
         }
     }
 
-    /**
-     * Applies friction to the player, called in both the air and on the ground
-     */
+    /// <summary>
+    /// Applies friction to the player, called in both the air and on the ground
+    /// </summary>
+    /// <param name="t"></param>
     private void ApplyFriction(float t)
     {
         Vector3 vec = playerVelocity; // Equivalent to: VectorCopy();
@@ -387,7 +372,6 @@ public class player_cpma : MonoBehaviour
         }
 
         newspeed = speed - drop;
-        playerFriction = newspeed;
 
         if (newspeed < 0)
         {
@@ -403,9 +387,9 @@ public class player_cpma : MonoBehaviour
         playerVelocity.z *= newspeed;
     }
 
-    /**
-     * Calculates wish acceleration based on player's cmd wishes
-     */
+    /// <summary>
+    /// Calculates wish acceleration based on player's cmd wishes
+    /// </summary>
     private void Accelerate(Vector3 wishdir, float wishspeed, float accel)
     {
         float addspeed;
@@ -436,37 +420,26 @@ public class player_cpma : MonoBehaviour
 
     }
 
-    private void OnGUI()
-    {
-        GUI.Label( new Rect(0, 0, 400, 100), "FPS: " + fps, style);
-        var ups = controller.velocity;
-        ups.y = 0;
-        GUI.Label(new Rect(0, 15, 400, 100), "Speed: " + Mathf.Round(ups.magnitude * 100) / 100 + "ups", style);
-        GUI.Label(new Rect(0, 30, 400, 100), "Top Speed: " + Mathf.Round(playerTopVelocity * 100) / 100 + "ups", style);
-    }
-
-    /*
-    ============
-    PM_CmdScale
-
-    Returns the scale factor to apply to cmd movements
-    This allows the clients to use axial -127 to 127 values for all directions
-    without getting a sqrt(2) distortion in speed.
-    ============
-    */
+    /// <summary>
+    /// PM_CmdScale
+    /// Returns the scale factor to apply to cmd movements
+    /// This allows the clients to use axial -127 to 127 values for all directions
+    /// without getting a sqrt(2) distortion in speed.
+    /// </summary>
+    /// <returns></returns>
     private float CmdScale()
     {
         int max;
         float total;
         float scale;
 
-        max = (int) Mathf.Abs(cmd.forwardmove);
+        max = (int)Mathf.Abs(cmd.forwardmove);
 
         if (Mathf.Abs(cmd.rightmove) > max)
         {
             max = (int)Mathf.Abs(cmd.rightmove);
         }
-        if (max != 0)
+        if (max == 0)
         {
             return 0;
         }
@@ -477,79 +450,22 @@ public class player_cpma : MonoBehaviour
         return scale;
     }
 
-
-    /**
-     * Plays a random jump sound
-     */
+    /// <summary>
+    /// Plays a random jump sound
+    /// </summary>
     private void PlayJumpSound()
     {
         // Don't play a new sound while the last hasn't finished
         var source = GetComponent<AudioSource>();
-
-        //if (audio.isPlaying)
-        //{
-        //    return;
-        //}
 
         if (source.isPlaying)
         {
             return;
         }
 
-        //audio.clip = jumpSounds[Random.Range(0, jumpSounds.length)];
-        //audio.Play();
+        var jumpSounds = jumpSoundsProvider.GetJumpSounds();
 
         source.clip = jumpSounds[Random.Range(0, jumpSounds.Length)];
         source.Play();
     }
-
-    //private void PlayerExplode()
-    //{
-    //    var velocity = controller.velocity;
-    //    velocity.Normalize();
-    //    var gibEffect = Instantiate(gibEffectPrefab, transform.position, Quaternion.identity);
-    //    gibEffect.GetComponent(GibFX).Explode(transform.position, velocity, controller.velocity.magnitude);
-    //    isDead = true;
-    //}
-
-    //private void PlayerSpawn()
-    //{
-    //    this.transform.position = playerSpawnPos;
-    //    this.playerView.rotation = playerSpawnRot;
-    //    rotX = 0.0;
-    //    rotY = 0.0;
-    //    playerVelocity = Vector3.zero;
-    //    isDead = false;
-    //}
-
-
-    // Legacy movement
-
-    // var wishdir : Vector3;
-    // var wishvel : float = airAcceleration;
-
-    // // var scale = CmdScale();
-
-    // SetMovementDir();
-
-    // /* If the player is just strafing in the air 
-    //    this simulates CPM (Not very accurately by
-    //    itself) */
-    // if(cmd.forwardmove == 0 && cmd.rightmove != 0)
-    // {
-    // 	wishvel = airStrafeAcceleration;
-    // }
-
-    // wishdir = Vector3(cmd.rightmove, 0, cmd.forwardmove);
-    // wishdir = transform.TransformDirection(wishdir);
-    // wishdir.Normalize();
-    // moveDirectionNorm = wishdir;
-
-    // var wishspeed = wishdir.magnitude;
-    // wishspeed *= moveSpeed;
-
-    // Accelerate(wishdir, wishspeed, wishvel);
-
-    // // Apply gravity
-    // playerVelocity.y -= gravity * Time.deltaTime;
 }
